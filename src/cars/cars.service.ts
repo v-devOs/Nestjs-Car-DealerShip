cb218f7bf72c9ca266b4252600d9889d2e34d30e
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-27 13:47:33 -0600

    Exception Filters

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CarsService {
@@ -26,6 +26,10 @@ export class CarsService {
   }
 
   findOneById( id: number ) {
-    return this.cars.find( car => car.id === id)
+    const car =  this.cars.find( car => car.id === id);
+
+    if( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
+    
+    return car;
   }
 }
