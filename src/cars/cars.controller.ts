9a0b72237f6438a3cecf568f1d99a4efa83bb506
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-27 13:39:56 -0600

    Pipes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -14,7 +14,7 @@ export class CarsController {
   }
 
   @Get(':id') //Entro a un segmento adicional
-  getCarById( @Param('id') id: string ) {
-    return this.carsService.findOneById( +id )
+  getCarById( @Param('id', ParseIntPipe ) id: number ) {
+    return this.carsService.findOneById( id )
   }
 }
