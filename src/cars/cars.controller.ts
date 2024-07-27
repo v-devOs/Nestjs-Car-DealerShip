7494ead15e1305293d714f60a697b3149f3d10b7
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-27 01:36:31 -0600

    Creando endpoint para segmentos de url

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+
+  private cars = ['Toyota', 'Honda', 'Jeep']
+  
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Jeep'];
+    return this.cars;
+  }
+
+  @Get(':id') //Entro a un segmento adicional
+  getCarById( @Param('id') id: string ) {
+    
+    return this.cars[id]
   }
 }
