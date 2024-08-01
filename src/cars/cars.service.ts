dcc25cbba54f2360910a128d62acb917b8e577fd
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-08-01 14:59:56 -0600

    Nuevo carro creado

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuid } from 'uuid'
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Injectable()
 export class CarsService {
@@ -33,5 +34,17 @@ export class CarsService {
     if( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
     
     return car;
+  }
+
+  create( createCarDto: CreateCarDto ) {
+
+    const newCar: Car = {
+      id: uuid(),
+      ...createCarDto
+    }
+
+    this.cars.push( newCar )
+
+    return newCar;
   }
 }
