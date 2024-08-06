e4b4c75b696c4aabcd70d0d677ce433a434e9a34
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-08-06 17:09:42 -0600

    Creacion del CRUD competo de Marcas

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,26 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import {v4 as uuid} from 'uuid'
 
 @Injectable()
 export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime()
+    }
+  ]
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+
+    const brand: Brand = {
+      id: uuid(),
+      name: createBrandDto.name.toLowerCase(),
+      createdAt: new Date().getTime()
+    }
+
+    this.brands.push( brand )
+
+    return brand;
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand = this.brands.find( brand => brand.id === id )
+
+    if( !brand ) throw new NotFoundException(`Brand with id ${ id } not found`)
+
+    return brand
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(
+    id: string, 
+    updateBrandDto: UpdateBrandDto
+  ) {
+    let brandDb = this.findOne( id );
+
+    this.brands = this.brands.map( brand => {
+      if( brand.id === id ){
+        brandDb.updatedAt = new Date().getTime(),
+        brandDb = { ...brandDb, ...updateBrandDto }
+
+        return brandDb;
+      }
+
+      return brand
+    })
+
+    return brandDb;
+    
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  remove(id: string) {
+    this.brands = this.brands.filter( brand => brand.id !== id)
   }
 }
